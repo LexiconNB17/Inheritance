89893aeac84bb5121e8a56cb647627457a1bf936
Adrian Lozano <adrian@xenotype.com>
2017-04-27 17:48:20 +0200

    F1rst commit

---
+++ Inheritance/Classes.cs
@@ -0,0 +1,43 @@
+﻿namespace Inheritance
+{
+    interface Interface1
+    {
+        string I1();
+    }
+    interface Interface2
+    {
+        int I1();
+        string I2();
+    }
+
+    abstract class AbstractClass {
+        public int AProp { get; set; }
+        public AbstractClass() { AProp = 1; }
+        public AbstractClass(int prop) { AProp = prop; }
+
+        public abstract string Abstract();
+        public virtual string Concrete() => "Abstract:Concrete";
+    }
+
+    class AClass : AbstractClass, Interface1
+    {
+        public AClass(int prop) : base(prop) { }
+
+        public virtual string Override() => "A:Override";
+        public string Hidden() => "A:Hidden";
+        public override string Abstract() => "A:Abstract";
+        public virtual string I1() => "A:I1";
+    }
+
+    class BClass : AClass, Interface2
+    {
+        public BClass() : base(3) { }
+
+        public override string Override() => "B:Override";
+        public new string Hidden() => "B:Hidden";
+        public override string Concrete() => "B:Concrete";
+        public string I2() => "B:I2";
+        public override string I1() => "B:I1";
+        int Interface2.I1() => 1;
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AClass a = new AClass(4);
+            BClass b = new BClass();
+            AClass c = b;
+            
+            WriteLine(a.Override());
+            WriteLine(b.Override());
+            WriteLine(c.Override());
+            WriteLine();
+
+            WriteLine(a.Hidden());
+            WriteLine(b.Hidden());
+            WriteLine(c.Hidden());
+            WriteLine();
+
+            WriteLine(a.Abstract());
+            WriteLine(b.Abstract());
+            WriteLine();
+
+            WriteLine(a.Concrete());
+            WriteLine(b.Concrete());
+            WriteLine();
+
+            WriteLine(b.I2());
+            WriteLine((c as BClass).I2());
+            WriteLine();
+
+            WriteLine(a.I1());
+            WriteLine(b.I1());
+            WriteLine(c.I1());
+            WriteLine((b as Interface2).I1());
+            WriteLine();
+
+        }
+    }
+}
+ 
